92ea9f4bd5eb69bf6bb5b89879b60dc8eb4e6b5f
brunch1896 <brunch_zhu@163.com>
2023-09-09 11:43:33 +0800

    imageCreation.py

--- imageCreation.py
+++ imageCreation.py
@@ -1,9 +1,26 @@
 import openai
+import requests
+from PIL import Image
+from io import BytesIO
 
+# 将以下YOUR_API_KEY替换为您的OpenAI API密钥
+openai.api_key = "YOUR_API_KEY"
+
+# 输入您的自然语言描述
+prompt = "可爱的小猫在草地上玩耍"
+
+# 请求DALL-E生成图像
 response = openai.Image.create(
-  prompt="A fluffy white cat with blue eyes sitting in a basket of flowers, looking up adorably at the camera",
-  n=1,
-  size="1024x1024"
+    prompt=prompt,
+    n=1,
+    size="512x512",
+    response_format="url"
 )
+
+# 获取图像URL
 image_url = response['data'][0]['url']
-print(image_url)
+
+# 下载并显示图像
+image_data = requests.get(image_url).content
+image = Image.open(BytesIO(image_data))
+image.show()
