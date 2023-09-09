c435862e057e92ac9cea30f211fec1ca5689ba79
brunch1896 <brunch_zhu@163.com>
2023-09-09 11:44:58 +0800

    imageCreation.py

--- imageCreation.py
+++ imageCreation.py
@@ -4,7 +4,8 @@ from PIL import Image
 from io import BytesIO
 
 # 将以下YOUR_API_KEY替换为您的OpenAI API密钥
-openai.api_key = "YOUR_API_KEY"
+# openai.api_key = "YOUR_API_KEY"
+openai.api_key = os.getenv('OPENAI_API_KEY') 
 
 # 输入您的自然语言描述
 prompt = "可爱的小猫在草地上玩耍"
